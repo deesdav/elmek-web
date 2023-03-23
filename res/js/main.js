89e86351eb2426ef8dd02707b702d2a4f2be0855
deesdav <105449474+deesdav@users.noreply.github.com>
2023-03-23 19:02:55 +0100

    update

--- res/js/main.js
+++ res/js/main.js
@@ -1,14 +1,12 @@
 const mainListDiv = document.getElementById("mainListDiv"),
-    mediaButton = document.getElementById("mediaButton");
+  mediaButton = document.getElementById("mediaButton");
 
 mediaButton.onclick = function () {
-    
-    "use strict";
-    
-    mainListDiv.classList.toggle("show_list");
-    mediaButton.classList.toggle("active");
-    
-}; 
+  "use strict";
+
+  mainListDiv.classList.toggle("show_list");
+  mediaButton.classList.toggle("active");
+};
 
 const menuHome = document.getElementById("menuHome");
 const menuCharakteristika = document.getElementById("menuCharakteristika");
@@ -20,48 +18,46 @@ const charakteristika = document.getElementById("charakteristika");
 const produkty = document.getElementById("produkty");
 const kontakty = document.getElementById("kontakty");
 
-
-
-menuHome.onclick = () =>{
-    home.style.display = "block";
-    charakteristika.style.display = "none";
-    produkty.style.display = "none";
-    kontakty.style.display = "none";
-    infoLesni.style.display = "none";
-    infoKvetovy.style.display = "none";
-    infoKvetovyPastovany.style.display = "none";
-    infoHorsky.style.display = "none";
-}
-menuCharakteristika.onclick = () =>{
-    home.style.display = "none";
-    charakteristika.style.display = "block";
-    produkty.style.display = "none";
-    kontakty.style.display = "none";
-    infoLesni.style.display = "none";
-    infoKvetovy.style.display = "none";
-    infoKvetovyPastovany.style.display = "none";
-    infoHorsky.style.display = "none";
-}
-menuProdukty.onclick = () =>{
-    home.style.display = "none";
-    charakteristika.style.display = "none";
-    produkty.style.display = "block";
-    kontakty.style.display = "none";
-    infoLesni.style.display = "none";
-    infoKvetovy.style.display = "none";
-    infoKvetovyPastovany.style.display = "none";
-    infoHorsky.style.display = "none";
-}
-menuKontakty.onclick = () =>{
-    home.style.display = "none";
-    charakteristika.style.display = "none";
-    produkty.style.display = "none";
-    kontakty.style.display = "block";
-    infoLesni.style.display = "none";
-    infoKvetovy.style.display = "none";
-    infoKvetovyPastovany.style.display = "none";
-    infoHorsky.style.display = "none";
-}
+menuHome.onclick = () => {
+  home.style.display = "block";
+  charakteristika.style.display = "none";
+  produkty.style.display = "none";
+  kontakty.style.display = "none";
+  infoLesni.style.display = "none";
+  infoKvetovy.style.display = "none";
+  infoKvetovyPastovany.style.display = "none";
+  infoHorsky.style.display = "none";
+};
+menuCharakteristika.onclick = () => {
+  home.style.display = "none";
+  charakteristika.style.display = "block";
+  produkty.style.display = "none";
+  kontakty.style.display = "none";
+  infoLesni.style.display = "none";
+  infoKvetovy.style.display = "none";
+  infoKvetovyPastovany.style.display = "none";
+  infoHorsky.style.display = "none";
+};
+menuProdukty.onclick = () => {
+  home.style.display = "none";
+  charakteristika.style.display = "none";
+  produkty.style.display = "block";
+  kontakty.style.display = "none";
+  infoLesni.style.display = "none";
+  infoKvetovy.style.display = "none";
+  infoKvetovyPastovany.style.display = "none";
+  infoHorsky.style.display = "none";
+};
+menuKontakty.onclick = () => {
+  home.style.display = "none";
+  charakteristika.style.display = "none";
+  produkty.style.display = "none";
+  kontakty.style.display = "block";
+  infoLesni.style.display = "none";
+  infoKvetovy.style.display = "none";
+  infoKvetovyPastovany.style.display = "none";
+  infoHorsky.style.display = "none";
+};
 
 const infoLesni = document.getElementById("infoLesni");
 const infoKvetovy = document.getElementById("infoKvetovy");
@@ -83,107 +79,101 @@ const two = document.getElementById("two");
 const three = document.getElementById("three");
 const four = document.getElementById("four");
 
-clicklesnik.onclick = () =>{
-    produkty.style.display = "none";
-    infoLesni.style.display = "block";
-    infoKvetovy.style.display = "none";
-    infoKvetovyPastovany.style.display = "none";
-    infoHorsky.style.display = "none";
-}
-clicklesnik.onmouseover = () =>{
-    medyphotos1.src="./res/img/lesnihover.png";
+clicklesnik.onclick = () => {
+  produkty.style.display = "none";
+  infoLesni.style.display = "block";
+  infoKvetovy.style.display = "none";
+  infoKvetovyPastovany.style.display = "none";
+  infoHorsky.style.display = "none";
+};
+clicklesnik.onmouseover = () => {
+        medyphotos1.src = "./res/img/lesnihover.png";
+};
+clicklesnik.onmouseout = () => {
+    medyphotos1.src = "./res/img/lesni.png";
+};
 
-}
-clicklesnik.onmouseout = () =>{
-    medyphotos1.src="./res/img/lesni.png";
-    
-}
-clickhornik.onclick = () =>{
-    produkty.style.display = "none";
-    infoLesni.style.display = "none";
-    infoKvetovy.style.display = "none";
-    infoKvetovyPastovany.style.display = "none";
-    infoHorsky.style.display = "block";
-}
-clickhornik.onmouseover = () =>{
-    medyphotos2.src="./res/img/horskyhover.png";
+clickhornik.onclick = () => {
+  produkty.style.display = "none";
+  infoLesni.style.display = "none";
+  infoKvetovy.style.display = "none";
+  infoKvetovyPastovany.style.display = "none";
+  infoHorsky.style.display = "block";
+};
+clickhornik.onmouseover = () => {
+    medyphotos2.src = "./res/img/horskyhover.png";
+};
+clickhornik.onmouseout = () => {
+    medyphotos2.src = "./res/img/horsky.png";
+};
+clickkvetovy.onclick = () => {
+  produkty.style.display = "none";
+  infoLesni.style.display = "none";
+  infoKvetovy.style.display = "block";
+  infoKvetovyPastovany.style.display = "none";
+  infoHorsky.style.display = "none";
+};
+clickkvetovy.onmouseover = () => {
+    medyphotos3.src = "./res/img/kvetovyhover.png";
+};
+clickkvetovy.onmouseout = () => {
+    medyphotos3.src = "./res/img/kvetovy.png";
+};
+clickkvetovypastovany.onclick = () => {
+  produkty.style.display = "none";
+  infoLesni.style.display = "none";
+  infoKvetovy.style.display = "none";
+  infoKvetovyPastovany.style.display = "block";
+  infoHorsky.style.display = "none";
+};
+clickkvetovypastovany.onmouseover = () => {
+    medyphotos4.src = "./res/img/kvetovypastovanyhover.png";
+};
+clickkvetovypastovany.onmouseout = () => {
+    medyphotos4.src = "./res/img/kvetovypastovany.png";
+};
 
-}
-clickhornik.onmouseout = () =>{
-    medyphotos2.src="./res/img/horsky.png";
-    
-}
-clickkvetovy.onclick = () =>{
-    produkty.style.display = "none";
-    infoLesni.style.display = "none";
-    infoKvetovy.style.display = "block";
-    infoKvetovyPastovany.style.display = "none";
-    infoHorsky.style.display = "none";
-}
-clickkvetovy.onmouseover = () =>{
-    medyphotos3.src="./res/img/kvetovyhover.png";
-
-}
-clickkvetovy.onmouseout = () =>{
-    medyphotos3.src="./res/img/kvetovy.png";
-    
-}
-clickkvetovypastovany.onclick = () =>{
-    produkty.style.display = "none";
-    infoLesni.style.display = "none";
-    infoKvetovy.style.display = "none";
-    infoKvetovyPastovany.style.display = "block";
-    infoHorsky.style.display = "none";
-}
-clickkvetovypastovany.onmouseover = () =>{
-    medyphotos4.src="./res/img/kvetovypastovanyhover.png";
-
-}
-clickkvetovypastovany.onmouseout = () =>{
-    medyphotos4.src="./res/img/kvetovypastovany.png";
-    
-}
-backButton.onclick = () =>{
-    home.style.display = "none";
-    charakteristika.style.display = "none";
-    produkty.style.display = "block";
-    kontakty.style.display = "none";
-    infoLesni.style.display = "none";
-    infoKvetovy.style.display = "none";
-    infoKvetovyPastovany.style.display = "none";
-    infoHorsky.style.display = "none";
-}
-two.onclick = () =>{
-    home.style.display = "none";
-    charakteristika.style.display = "none";
-    produkty.style.display = "block";
-    kontakty.style.display = "none";
-    infoLesni.style.display = "none";
-    infoKvetovy.style.display = "none";
-    infoKvetovyPastovany.style.display = "none";
-    infoHorsky.style.display = "none";
-}
-three.onclick = () =>{
-    home.style.display = "none";
-    charakteristika.style.display = "none";
-    produkty.style.display = "block";
-    kontakty.style.display = "none";
-    infoLesni.style.display = "none";
-    infoKvetovy.style.display = "none";
-    infoKvetovyPastovany.style.display = "none";
-    infoHorsky.style.display = "none";
-}
-four.onclick = () =>{
-    home.style.display = "none";
-    charakteristika.style.display = "none";
-    produkty.style.display = "block";
-    kontakty.style.display = "none";
-    infoLesni.style.display = "none";
-    infoKvetovy.style.display = "none";
-    infoKvetovyPastovany.style.display = "none";
-    infoHorsky.style.display = "none";
-}
+backButton.onclick = () => {
+  home.style.display = "none";
+  charakteristika.style.display = "none";
+  produkty.style.display = "block";
+  kontakty.style.display = "none";
+  infoLesni.style.display = "none";
+  infoKvetovy.style.display = "none";
+  infoKvetovyPastovany.style.display = "none";
+  infoHorsky.style.display = "none";
+};
+two.onclick = () => {
+  home.style.display = "none";
+  charakteristika.style.display = "none";
+  produkty.style.display = "block";
+  kontakty.style.display = "none";
+  infoLesni.style.display = "none";
+  infoKvetovy.style.display = "none";
+  infoKvetovyPastovany.style.display = "none";
+  infoHorsky.style.display = "none";
+};
+three.onclick = () => {
+  home.style.display = "none";
+  charakteristika.style.display = "none";
+  produkty.style.display = "block";
+  kontakty.style.display = "none";
+  infoLesni.style.display = "none";
+  infoKvetovy.style.display = "none";
+  infoKvetovyPastovany.style.display = "none";
+  infoHorsky.style.display = "none";
+};
+four.onclick = () => {
+  home.style.display = "none";
+  charakteristika.style.display = "none";
+  produkty.style.display = "block";
+  kontakty.style.display = "none";
+  infoLesni.style.display = "none";
+  infoKvetovy.style.display = "none";
+  infoKvetovyPastovany.style.display = "none";
+  infoHorsky.style.display = "none";
+};
 
 setTimeout(() => {
-    document.body.style.display = "block";
+  document.body.style.display = "block";
 }, 900);
